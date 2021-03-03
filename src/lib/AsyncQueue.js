02985eaab9abddc4c8453c49d275ba8575705ce1
Marco Di Benedetto <Marco.DiBenedetto@riverbed.com>
2021-03-02 17:45:48 -0800

    Added support for multiple priorities

--- src/lib/AsyncQueue.js
+++ src/lib/AsyncQueue.js
@@ -4,7 +4,7 @@ Classes.AsyncQueue = Classes.Base.subclass({
 
 	_eventManager: null,
 
-	_queue: null,
+	_queues: null,
 
 	// This class batches the processing based on entries count, not based on elapsed
 	// time. Using elapsed time would give us a more accurate balancing of the processing
@@ -31,7 +31,8 @@ _init: function() {
 	this._eventManager = Classes.EventManager.create();
 	this._eventManager.attachRegistrationFunctions(this);
 
-	this._queue = [];
+	this._initQueues();
+
 	this._running = false;
 	this._discarded = false;
 },
@@ -42,6 +43,59 @@ _isRunning: function() {
 
 _isDiscarded: function() {
 	return this._discarded;
+},
+
+_initQueues: function() {
+	this._queues = {}
+	this._queues[Classes.AsyncQueue.priority.HIGH] = {
+		name: Classes.AsyncQueue.priority.HIGH,
+		delay: 0, // in ms
+		queue: [],
+	};
+	this._queues[Classes.AsyncQueue.priority.NORMAL] = {
+		name: Classes.AsyncQueue.priority.NORMAL,
+		delay: 0, // in ms
+		queue: [],
+	};
+	this._queues[Classes.AsyncQueue.priority.LOW] = {
+		name: Classes.AsyncQueue.priority.LOW,
+		delay: 200, // in ms
+		queue: [],
+	};
+},
+
+_getQueue: function(priority) {
+	return this._queues[priority];
+},
+
+_getQueueToProcess: function() {
+	const logHead = "AsyncQueue::_getQueueToProcess(): ";
+
+	let priorities = [
+		Classes.AsyncQueue.priority.HIGH,
+		Classes.AsyncQueue.priority.NORMAL,
+		Classes.AsyncQueue.priority.LOW
+	];
+
+	// We're using a strict prority strategy, nothing gets served from the lower priority
+	// queue if the higher priority queue has work pending.
+	for(priority of priorities) {
+		if(this._queues[priority].queue.length != 0) {
+			this._log(logHead + "queue of priority \"" + priority + "\" has pending work: " +
+						this._queues[priority].queue.length);
+			return this._queues[priority];
+		}
+	}
+	this._log(logHead + "all queues are empty");
+	return null;
+},
+
+_getAllQueueLengths: function() {
+	let retVal = [];
+	for(const [ priority, queue ] of Object.entries(this._queues)) {
+		retVal.push(priority + ": " + queue.queue.length);
+	}
+	return retVal;
 },
 
 // "processFn" cannot be async, and it's mandatory.
@@ -74,46 +128,57 @@ _processQueue: async function(perfLabel, processFn, resumeFn, cleanupFn) {
 	// above, we're starting as if we were ending an iteration, but we want to
 	// correct that for the purposes of perfProf.
 	let firstRound = true;
+	let queue = null;
 
-	while(this._queue.length > 0) {
-		if(processedCnt >= this._batchSize) {
-			if(!firstRound) {
-				perfProf.mark(perfLabel + "AsyncQueueBatchEnd" + this._id);
-				// By putting the "delay(0)" here, we avoid it getting called
-				// during the first iteration of _processQueue(), making it
-				// a bit more responsive... except that this way it won't have
-				// had time to get much enqueued, and it will do very little
-				// the first time.
-				await delay(0);
-			} else {
-				firstRound = false;
-			}
-
-//			this._log(logHead + "pausing, queue length = " + this._queue.length);
-			// Wait for the next event cycle to resume processing
-			//await delay(0);
-//			this._log(logHead + "resuming, queue length = " + this._queue.length);
-
-			processedCnt = 0;
-			// Since we waited for the next event cycle, anything might have happened
-			// while we were waiting, did this AsyncQueue get discarded?
-			if(resumeFn != null && !resumeFn()) {
-				// We got discarded, so we've decided we need to stop iterating
-				if(cleanupFn != null) {
-					await cleanupFn();
+	// To avoid completely starving the lower priority queues, we try to fill a full
+	// _batchSize cycle with queue entries from lower priority queues if the higher
+	// priority queue empties (before we go to the next event cycle, and maybe in
+	// the meantime more high priority items are added)
+	while((queue = this._getQueueToProcess()) != null) {
+		while(queue.queue.length > 0) {
+			if(processedCnt >= this._batchSize) {
+				if(!firstRound) {
+					perfProf.mark(perfLabel + "AsyncQueueBatchEnd" + this._id);
+					// By putting the "delay(0)" here, we avoid it getting called
+					// during the first iteration of _processQueue(), making it
+					// a bit more responsive... except that this way it won't have
+					// had time to get much enqueued, and it will do very little
+					// the first time.
+					await delay(queue.delay);
+				} else {
+					firstRound = false;
 				}
-				// Signal the caller that we don't want to be called back again from
-				// the outer loop (in case cleanupFn() did not empty the queue...)
-				return false;
+
+	//			this._log(logHead + "pausing, queue length = " + queue.queue.length);
+				// Wait for the next event cycle to resume processing
+				//await delay(queue.delay);
+	//			this._log(logHead + "resuming, queue length = " + queue.queue.length);
+
+				processedCnt = 0;
+				// Since we waited for the next event cycle, anything might have happened
+				// while we were waiting, did this AsyncQueue get discarded?
+				if(resumeFn != null && !resumeFn()) {
+					// We got discarded, so we've decided we need to stop iterating
+					if(cleanupFn != null) {
+						await cleanupFn();
+					}
+					// Signal the caller that we don't want to be called back again from
+					// the outer loop (in case cleanupFn() did not empty the queue...)
+					return false;
+				}
+				perfProf.mark(perfLabel + "AsyncQueueBatchStart" + this._id);
 			}
-			perfProf.mark(perfLabel + "AsyncQueueBatchStart" + this._id);
+
+			let entry = queue.queue.shift();
+			// We assume the function being called doesn't need to be synchronized
+			// (it's either synchronous, or, if async, can complete later). We don't
+			// want to add an "await" here, because returning from "await" needs to
+			// wait for the next event cycle, and that would make this processing
+			// extremely slow.
+			processFn(entry);
+			processedCnt++;
 		}
-
-		let entry = this._queue.shift();
-		processFn(entry);
-		processedCnt++;
 	}
-
 	perfProf.mark(perfLabel + "AsyncQueueBatchEnd" + this._id);
 	return true;
 },
@@ -136,7 +201,7 @@ _runProcessFn: function(entry) {
 
 _runCleanupFn: async function() {
 	const logHead = "AsyncQueue::_runCleanupFn(): ";
-	this._log(logHead + "entering, queue length = " + this._queue.length);
+	this._log(logHead + "entering, queue lengths = ", this._getAllQueueLengths());
 
 	await this._processQueue("discard", this._discardProcessFn.bind(this));
 },
@@ -168,7 +233,7 @@ _runQueue: async function() {
 		// the decision of _processQueue() to come back in "normalState", we
 		// must revalidate if the queue is empty or not.
 		// _processQueue() at least once more
-		while(this._queue.length > 0 && normalState) {
+		while(this._getQueueToProcess() != null && normalState) {
 			normalState = await this._processQueue("run", this._runProcessFn.bind(this),
 												this._runResumeFn.bind(this), this._runCleanupFn.bind(this));
 		}
@@ -182,15 +247,16 @@ _runQueue: async function() {
 	this._running = false;
 },
 
-_enqueueInner: function(queueEntry) {
-	this._queue.push(queueEntry);
+_enqueueInner: function(queueEntry, priority) {
+	this._getQueue(priority).queue.push(queueEntry);
 	this._runQueue();
 },
 
 // "fnSignature" is for debugging
-enqueue: function(fn, fnSignature) {
+enqueue: function(fn, fnSignature, priority) {
 //	const logHead = "AsyncQueue::enqueue(" + fnSignature + "): ";
 
+	priority = optionalWithDefault(priority, Classes.AsyncQueue.priority.NORMAL);
 	return new Promise(
 		function(resolveFn, rejectFn) {
 //			this._log(logHead + "enqueueing");
@@ -205,7 +271,7 @@ enqueue: function(fn, fnSignature) {
 				// of the queue (if they have one).
 				discardFn: null,
 			};
-			this._enqueueInner(queueEntry);
+			this._enqueueInner(queueEntry, priority);
 		}.bind(this)
 	);
 },
@@ -220,12 +286,17 @@ discard: async function() {
 
 	if(!this._isRunning()) {
 		// Unlikely there's anything to cleanup if it's not running...
-		this._assert(this._queue.length == 0);
+		this._assert(this._getQueueToProcess() == null);
 		// But anyway, just in case, perform the cleanup
-		if(this._queue.length > 0) {
+		if(this._getQueueToProcess() != null) {
 			await this._runCleanupFn();
 		}
 	}
 }
 
 }); // Classes.AsyncQueue
+
+Classes.Base.roDef(Classes.AsyncQueue, "priority", {} );
+Classes.Base.roDef(Classes.AsyncQueue.priority, "HIGH", "high" );
+Classes.Base.roDef(Classes.AsyncQueue.priority, "NORMAL", "normal" );
+Classes.Base.roDef(Classes.AsyncQueue.priority, "LOW", "low" );
