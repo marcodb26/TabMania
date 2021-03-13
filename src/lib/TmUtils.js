d89a5ee82f2a4602a0e9535ea97e8c184ba9b462
Marco Di Benedetto <Marco.DiBenedetto@riverbed.com>
2021-03-12 17:05:31 -0800

    - Added function to perform a comparison test between the performance of concat(),   push() and a vanilla for() loop - Tiny performance improvement for tmUtils.deepClone()

--- src/lib/TmUtils.js
+++ src/lib/TmUtils.js
@@ -121,7 +121,9 @@ deepClone: function(obj) {
 	// obj is a non-null object...
 
 	if(Array.isArray(obj)) {
-		let retVal = [];
+		// Apparently initializing the array to the size it needs is faster than growing
+		// the array one element by one
+		let retVal = Array(obj.length);
 		for(let i = 0; i < obj.length; i++) {
 			retVal[i] = this.deepClone(obj[i]);
 		}
@@ -185,6 +187,92 @@ showSearchParserInfo: function() {
 
 showBookmarksStats: function() {
 	console.log("bookmarksManager statistics:", bookmarksManager.getStats());
+},
+
+// Trying to validate results from https://dev.to/uilicious/javascript-array-push-is-945x-faster-than-array-concat-1oki
+// Outcome on 21.03.12: nope, concat() is faster, though when you have:
+// - 15,000 arrays of 5 elements (as indicated on that blog):
+//   * concat(): 183.3ms (only 1.5x faster than push() )
+//   * push(): 269.1ms
+//   * appendArray(): 457.6ms
+// - 1,000 arrays of 100 elements:
+//   * concat(): 76.9ms (2.9x faster than push() )
+//   * push(): 225.4ms
+//   * appendArray(): 206.7ms
+testConcatPush: function() {
+	// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+	function getRandomInt(max) {
+		return Math.floor(Math.random() * Math.floor(max));
+	}
+
+	function createArrays(arrayCnt, arraySize) {
+		let retVal = Array(arrayCnt);
+		for(let i = 0; i < arrayCnt; i++) {
+			retVal[i] = Array(arraySize);
+			for(let j = 0; j < arraySize; j++) {
+				retVal[i][j] = getRandomInt(100);
+			}
+		}
+		return retVal;
+	}
+
+	function appendArray(dst, src) {
+		let dstLength = dst.length;
+		let srcLength = src.length;
+		dst.length = dstLength + srcLength;
+
+		for(let i = 0; i < srcLength; i++) {
+			dst[dstLength + i] = src[i];
+		}
+	}
+
+	let maxRepeat = 100;
+	let origArray = createArrays(1000, 100);
+
+	perfProf.mark("testConcatPush_concatStart");
+	let concatResult = null;
+	for(let r = 0; r < maxRepeat; r++) {
+		concatResult = [].concat.apply([], origArray);
+	}
+	perfProf.mark("testConcatPush_concatEnd");
+
+	perfProf.mark("testConcatPush_pushStart");
+	let pushResult = null;
+	for(let r = 0; r < maxRepeat; r++) {
+		pushResult = [];
+		for(let i = 0; i < origArray.length; i++) {
+			pushResult.push.apply(pushResult, origArray[i]);
+		}
+	}
+	perfProf.mark("testConcatPush_pushEnd");
+
+	perfProf.mark("testConcatPush_appendStart");
+	let appendResult = null;
+	for(let r = 0; r < maxRepeat; r++) {
+		appendResult = [];
+		for(let i = 0; i < origArray.length; i++) {
+			appendArray(appendResult, origArray[i]);
+		}
+	}
+	perfProf.mark("testConcatPush_appendEnd");
+
+	if(!this.isEqual(concatResult, pushResult)) {
+		this._err("Invalid result, concat() and push() generated different output arrays");
+		return;
+	}
+	if(!this.isEqual(concatResult, appendResult)) {
+		this._err("Invalid result, concat() and appendArray() generated different output arrays");
+		return;
+	}
+
+	console.log("Valid result, concat(), push() and appendArray() generated the same output");
+	let toMeasure = {
+		"concat": [ "testConcatPush_concatStart", "testConcatPush_concatEnd" ],
+		"push": [ "testConcatPush_pushStart", "testConcatPush_pushEnd" ],
+		"append": [ "testConcatPush_appendStart", "testConcatPush_appendEnd" ],
+	};
+
+	perfProf.showMeasures(toMeasure);
 },
 
 }); // Classes.TmUtils
