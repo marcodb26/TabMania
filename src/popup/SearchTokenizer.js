6caa9947fae12626da4e04aab8a78c682dd36cb3
Marco Di Benedetto <Marco.DiBenedetto@riverbed.com>
2021-03-15 21:21:34 -0700

    - Fixed the escaping story: escaping at top level, in quoted string and in unquoted regex   are 3 different things, and we should do minimal escaping for each context. E.g. in   quoted string you only need to escape the matching quote, not all escapable characters,   and you should consider \ escaped only as \\” while any other occurrence of \\ should   be considered two \
    - Fixed search query delimiter story: now """, "'", "(", " " and "\t" are unified
      as delimiters (unless you're in a quoted string or unquoted regex string) and
      cause the previous token to be considered complete
    
    - Fixed error message for invalid regex in search query
      * Now we show the regex parse error on the console (for /dist users)

--- src/popup/SearchTokenizer.js
+++ src/popup/SearchTokenizer.js
@@ -6,6 +6,8 @@ Classes.SearchTokenizer = Classes.Base.subclass({
 	// debugging messages for the parser can be very verbose
 	_parserDebug: null,
 
+	// Don't include "r" here, "r:" is not an operator, it's a tokenization symbol
+	// like a quote or a parenthesis
 	_validUnaryOpList: [
 		"-", // The negation operator
 		"site",   // Check only the hostname of the URL
@@ -15,7 +17,12 @@ Classes.SearchTokenizer = Classes.Base.subclass({
 		"ingroup",
 	],
 
-	_escapedCharsList: [ "\"", "\'", "\\", ":", "-", " ", "(", ")" ],
+	// List here all the token delimiters that need to be escaped. This list
+	// applies only for "standard" tokenization, not for quoted strings or
+	// unquoted regex strings tokenization, see _consumeEscapedCharacter().
+	// Don't include "\\" in this list, it requires special treatment and it's
+	// handled by _consumeEscapedCharacter().
+	_escapedCharsList: [ "\"", "\'", ":", " ", "\t", "(", ")" ],
 
 // "value" is optional
 _init: function(value) {
@@ -28,17 +35,53 @@ _init: function(value) {
 	this._parserDebug = emptyFn;
 },
 
-_consumeEscapedCharacter: function(queryString) {
+_nextCharIsEscapable: function(queryString, escapedCharsList) {
+	// This function assumes the caller has already checked "queryString.length > 0"
+
+	// Look ahead what follows the escape character that triggered the caller to
+	// call this function
+	let nextChar = queryString[0];
+
+	if(escapedCharsList.includes(nextChar)) {
+		return true;
+	}
+
+	if(nextChar == "\\" && queryString.length > 1 && escapedCharsList.includes(queryString[1])) {
+		// The escape character "\" is escapable only if it's followed by a delimiter, to
+		// allow delimiters to behave as delimiters when a quoted string or an unquoted
+		// regex string needs to end with a "\" (e.g. "\\ " or "\\'").
+		// This means that if you need to represent a "\" followed by a " ", you can't
+		// do it with an unquoted regex string, you must use a quoted string (where the
+		// space doesn't behave as delimiter and doesn't require to be escaped), and
+		// viceversa you can't represent a "\" followed by a "'" in a quoted string,
+		// you must use an alternate quote (you can't use an unquoted string because
+		// the quoted character is still a delimited in an unquoted string).
+		// If you need to represent both "\" followed by "'" and "\" followed by """
+		// you just can't, sorry about that... let's see if this limitation becomes
+		// a problem...
+		return true;
+	}
+
+	return false;
+},
+
+// "escapedCharsList" is optional. By default, we escape all the characters that can
+// represent token delimiters. Specify a non-default "escapedCharsList" if you need
+// to escape a different set of token delimiters (e.g. for quoted strings or unquoted
+// regex strings).
+_consumeEscapedCharacter: function(queryString, escapedCharsList) {
+	escapedCharsList = optionalWithDefault(escapedCharsList, this._escapedCharsList);
+
+	if(queryString.length == 0 || !this._nextCharIsEscapable(queryString, escapedCharsList)) {
+		// "\" at the end of the string or not escaping a delimiter, consume as-is
+		return [ queryString, "\\" ];
+	}
+
+	// Consume the escaped delimiter/escape
 	let currChar = queryString[0];
 	// Shift one character to the left
 	queryString = queryString.substring(1);
-
-	if(this._escapedCharsList.includes(currChar)) {
-		return [ queryString, currChar ];
-	}
-
-	// Not escaping anything meaningful, put back the "\" in the output
-	return [ queryString, "\\" + currChar ];
+	return [ queryString, currChar ];
 },
 
 _consumeQuotedString: function(queryString, matchingQuoteChar) {
@@ -54,11 +97,13 @@ _consumeQuotedString: function(queryString, matchingQuoteChar) {
 		switch(currChar) {
 			case "\\":
 				let escapedString = "";
-				[ queryString, escapedString ] = this._consumeEscapedCharacter(queryString);
+				let escapedCharsList = this.getEscapedCharsList(Classes.SearchTokenizer.type.QUOTEDTEXT, matchingQuoteChar);
+				[ queryString, escapedString ] = this._consumeEscapedCharacter(queryString, escapedCharsList);
 				token += escapedString;
 				break;
 
 			case matchingQuoteChar:
+				// End of a quoted string
 				return [ queryString, token ];
 
 			default:
@@ -72,8 +117,8 @@ _consumeQuotedString: function(queryString, matchingQuoteChar) {
 	return [ queryString, token ];
 },
 
-_consumeRegex: function(queryString) {
-	const logHead = "SearchTokenizer::_consumeRegex(\"" + queryString + "\"): ";
+_consumeRegexString: function(queryString) {
+	const logHead = "SearchTokenizer::_consumeRegexString(\"" + queryString + "\"): ";
 
 	let token = "";
 
@@ -85,13 +130,14 @@ _consumeRegex: function(queryString) {
 		switch(currChar) {
 			case "\\":
 				let escapedString = "";
-				[ queryString, escapedString ] = this._consumeEscapedCharacter(queryString);
+				let escapedCharsList = this.getEscapedCharsList(Classes.SearchTokenizer.type.REGEX);
+				[ queryString, escapedString ] = this._consumeEscapedCharacter(queryString, escapedCharsList);
 				token += escapedString;
 				break;
 
 			case " ":
 			case "\t":
-				// Whitespaces mark the end of a regex
+				// Whitespaces mark the end of an unquoted regex string
 				return [ queryString, token ];
 
 			case "\"":
@@ -100,6 +146,7 @@ _consumeRegex: function(queryString) {
 					// A quote in the middle of a regex is just part of that regex
 					token += currChar;
 				} else {
+					// Consume a quoted regex string
 					return this._consumeQuotedString(queryString, currChar);
 				}
 				break;
@@ -115,25 +162,38 @@ _consumeRegex: function(queryString) {
 },
 
 _setRegexToken: function(token) {
+	if(token.length == 0) {
+		// If the token is "", the consumed queryString likely was r:"", so we need to drop/ignore.
+		return null;
+	}
+
 	let regex = null;
 	let regexErr = null;
 
 	let retVal = {
 		type: Classes.SearchTokenizer.type.REGEX,
+		sources: [ token ],
 	};
 
 	try {
 		// The flag "i" means "ignoreCase"
 		retVal.value = new RegExp(token, "i");
-		retVal.textValue = token;
 	} catch(e) {
-		this._log(logHead + "unable to parse regex", e);
+		const logHead = "SearchTokenizer::_setRegexToken(): ";
+		this._log(logHead + "unable to parse regex for token /" + token + "/: ", e);
+		// In production, RegExp syntax errors will be very hard to catch, best to at least
+		// show the raw parse error message on the console.
+		// We choose to avoid dumping the entire stack with "this._err(e)", and instead
+		// selectively report only the error message (though the stack can still be seen
+		// by expandind the error message on the console, at least it won't be vomited
+		// right on the console).
+		console.error("RegExp parser: " + e.name + ": " + e.message);
 		retVal.error = e;
 	}
 	return retVal;
 },
 
-_setTextOrBinaryOp: function(token) {
+_setTextOrBinaryOpToken: function(token) {
 	switch(token) {
 		case "and":
 		case "or":
@@ -142,12 +202,68 @@ _setTextOrBinaryOp: function(token) {
 	return { type: Classes.SearchTokenizer.type.TEXT, value: token };
 },
 
+_setUnaryOpToken: function(token) {
+	if(this._validUnaryOpList.includes(token)) {
+		return { type: Classes.SearchTokenizer.type.UNARYOP, value: token };
+	} else {
+		const logHead = "SearchTokenizer::_setUnaryOpToken(): ";
+		this._log(logHead + "discarding unknown unary operator \"" + token + "\"");
+		return null;
+	}
+},
+
+// Returns a token object, or "null" if the token should be discarded
+_genTokenByType: function(token, tokenType) {
+	// Note that we can't always "token.toLowerCase()", because the REGEX type must
+	// preserve the case of the token text
+	switch(tokenType) {
+		case Classes.SearchTokenizer.type.TEXT:
+			return this._setTextOrBinaryOpToken(token.toLowerCase());
+
+		case Classes.SearchTokenizer.type.REGEX:
+			// Don't toLowerCase() the token for regex, since things like "\w" and "\W"
+			// mean two different things. We'll need to make the regex case insensitive
+			// in other ways (see _setRegexToken()).
+			return this._setRegexToken(token);
+
+		case Classes.SearchTokenizer.type.UNARYOP:
+			return this._setUnaryOpToken(token.toLowerCase());
+
+		case Classes.SearchTokenizer.type.QUOTEDTEXT:
+			// No need to call _setTextOrBinaryOp(), a quoted "and" or "or" is just text
+			return { type: Classes.SearchTokenizer.type.TEXT, value: token.toLowerCase() };
+
+		case Classes.SearchTokenizer.type.SUBTREE:
+		case Classes.SearchTokenizer.type.BINARYOP:
+		default:
+			// Note that _genTokenByType() should not be called for tokenType
+			// Classes.SearchTokenizer.type.SUBTREE and Classes.SearchTokenizer.type.BINARYOP.
+			// Classes.SearchTokenizer.type.BINARYOP is determined under Classes.SearchTokenizer.type.TEXT,
+			// while Classes.SearchTokenizer.type.SUBTREE is a special case handled in tokenize().
+			const logHead = "SearchTokenizer::_genTokenByType(): ";
+			this._err(logHead + "invalid or unknown tokenType \"" + tokenType + "\"");
+	}
+
+	// Discard unknown tokenType
+	return null;
+},
+
 tokenize: function(queryString, tokenList, topLevel) {
 	topLevel = optionalWithDefault(topLevel, true);
 	const logHead = "SearchTokenizer::tokenize(\"" + queryString + "\", topLevel: " + topLevel + "): ";
 
 	let token = "";
-	let tokenType = Classes.SearchTokenizer.type.TEXT;
+	
+	let generateToken = function(tokenType) {
+		if(token.length > 0) {
+			let tokenObj = this._genTokenByType(token, tokenType);
+			if(tokenObj != null) {
+				tokenList.push(tokenObj);
+			}
+			token = "";
+		}
+		// We just drop empty tokens, nothing to do
+	}.bind(this);
 
 	while(queryString.length > 0) {
 		let currChar = queryString[0];
@@ -159,30 +275,14 @@ tokenize: function(queryString, tokenList, topLevel) {
 		switch(currChar) {
 			case " ":
 			case "\t":
-				if(token.length > 0) {
-					tokenList.push(this._setTextOrBinaryOp(token.toLowerCase()));
-					token = "";
-				}
+				generateToken(Classes.SearchTokenizer.type.TEXT);
 				break;
 
 			case "\"":
 			case "\'":
-				if(token.length > 0) {
-					// A quote in the middle of a token is just part of that token, we recognize
-					// a quoted string only if the leading quote is preceded by any token
-					// delimiter (space, ":", "-"), which consumes the previous token and
-					// resets token to "".
-					// Note that we play by different rules for the closing quote, so while
-					// a"bc is a single token, "ab"c will be two tokens ab and c
-					token += currChar;
-				} else {
-					[ queryString, token ] = this._consumeQuotedString(queryString, currChar);
-					if(token.length != 0) {
-						// No need to call _setTextOrBinaryOp(), a quoted "and" or "or" is just text
-						tokenList.push({ type: Classes.SearchTokenizer.type.QUOTEDTEXT, value: token.toLowerCase() });
-						token = "";
-					}
-				}
+				generateToken(Classes.SearchTokenizer.type.TEXT);
+				[ queryString, token ] = this._consumeQuotedString(queryString, currChar);
+				generateToken(Classes.SearchTokenizer.type.QUOTEDTEXT);
 				break;
 
 			case "\\":
@@ -191,68 +291,57 @@ tokenize: function(queryString, tokenList, topLevel) {
 				token += escapedString;
 				break;
 
-			case ":":
-				if(token == "r") {
-					// Regex case
-					if(queryString.length == 0 || [ " ", "\t" ].includes(queryString[0])) {
-						// "r:" is at the end of the queryString, or it's followed by a whitespaces
-						// (which is the delimiter of a non-quoted regex): in this case, we treat
-						// "r: as regular text.
-						// The processing of the next character (or of the end of the string) will
-						// deal with it, so nothing to do in this block, except accumulating.
-						token += currChar;
-						break;
-					}
-					[ queryString, token ] = this._consumeRegex(queryString);
-					if(token.length != 0) {
-						// Don't toLowerCase() the token for regex, since things like "\w" and "\W"
-						// mean two different things. We'll need to make the regex case insensitive
-						// in other ways (see _setRegexToken()).
-						tokenList.push(this._setRegexToken(token));
-					} else {
-						// This is different from the first if() above. If the token is "",
-						// the consumed queryString likely was r:"", so we need to drop/ignore.
-					}
-					token = "";
-					break;
-				}
-				// Don't use "break;" here, if "token" was not "r", then ":" is the unary operator and
-				// behaves like "-" below
 			case "-":
+				token += "-";
+				// No break here, we just needed to normalize the "-" operator with the "<token>:"
+				// operator: in the latter case <token> is already in "token" (since we're now
+				// processing ":", while in case of "-" we need to add "-" to "token" here
+			case ":":
 				// First line of the "if" block:
 				// - Look ahead to see if this candidate operator is followed by a space (or
 				//   it's at the end of the queryString), in which case we'll treat this as
 				//   regular text, not as an operator.
+				//   "r:" is not an operator, but for tokenization purposes here it behaves
+				//   like one: if "r:" is at the end of the queryString, or it's followed by a
+				//   whitespaces (which is the delimiter of a non-quoted regex), then we treat
+				//   "r:" as regular text.
 				// Second line of the "if" block:
-				// - As an extra condition, if the candidate operator is "-", it's a valid
-				//   operator only if no token has been accumulated so far, otherwise we should
-				//   treat it as regular text (that is, in "a -b" the "-" is an operator, while
-				//   in "a-b" we have a single token "a-b" and no operator.
-				if((queryString.length > 0 && ![ " ", "\t" ].includes(queryString[0])) &&
-					!(currChar == "-" && token.length != 0)) {
-					// This is a real operator
-					if(currChar == "-") {
-						// There's no accumulated token for "-", let's make one up
-						token = "-";
+				// - As an extra condition, if the candidate operator is not a known unary
+				//   operator or "r:", then again treat it as regular text
+				//   * Note also that if the candidate operator is "-", it's a valid operator
+				//     only if no token has been accumulated before "-", otherwise we should
+				//     treat it as regular text: that is, in "a -b" the "-" is an operator, while
+				//     in "a-b" we have a single token "a-b" and no operator. This is covered
+				//     within the second line check, because if we have "a-b", then "token"
+				//     right now contains "a-", which is not a valid unary operator.
+				if(queryString.length == 0 || [ " ", "\t" ].includes(queryString[0]) ||
+					!(this._validUnaryOpList.includes(token) || token == "r" )) {
+					// The processing of the next character (or of the end of the string) will
+					// deal with it, so nothing to do in this block, except accumulating in the
+					// case of ":" ("-" has already been added to "token").
+					if(currChar == ":") {
+						token += currChar;
 					}
-					let lowerCaseToken = token.toLowerCase();
-					if(this._validUnaryOpList.includes(lowerCaseToken)) {
-						tokenList.push({ type: Classes.SearchTokenizer.type.UNARYOP, value: lowerCaseToken });
-					} else {
-						this._log(logHead + "discarding unknown unary operator \"" + token + "\"");
-					}
-					token = "";
-				} else {
-					// This is regular text, not an operator. The processing of the
-					// next character (or of the end of the string) will deal with it,
-					// so nothing to do in this block, except accumulating.
-					token += currChar;
+					break;
 				}
+				if(token == "r") {
+					// Regex case, not a unary operator
+					[ queryString, token ] = this._consumeRegexString(queryString);
+					generateToken(Classes.SearchTokenizer.type.REGEX);
+					break;
+				}
+				// This is a real operator
+				generateToken(Classes.SearchTokenizer.type.UNARYOP);
 				break;
 
 			case "(":
+				// "(", """ and "'" are all token delimiters, any token text accumulated so far
+				// must be considered a full token.
+				generateToken(Classes.SearchTokenizer.type.TEXT);
 				let subTokenList = [];
 				queryString = this.tokenize(queryString, subTokenList, false);
+				// Can't call generateToken() for subtrees, as generateToken() assumes it needs
+				// to use "token", while here we need to use "subTokenList", not "token"
 				if(subTokenList.length != 0) {
 					tokenList.push({ type: Classes.SearchTokenizer.type.SUBTREE, value: subTokenList });
 				}
@@ -260,12 +349,7 @@ tokenize: function(queryString, tokenList, topLevel) {
 
 			case ")":
 				if(!topLevel) {
-					if(token.length > 0) {
-						// In theory, no need to call _setTextOrBinaryOp(), as an "and" or "or" at the end of
-						// the parentheses block can't be a binary operator. On the other hand, we'd like to
-						// catch this case in a log (see the push() outside of the while() loop for more details.
-						tokenList.push(this._setTextOrBinaryOp(token.toLowerCase()));
-					}
+					generateToken(Classes.SearchTokenizer.type.TEXT);
 					return queryString;
 				}
 				// If this is the outermost tokenize(), we just found an unbalanced right parenthesis,
@@ -280,18 +364,35 @@ tokenize: function(queryString, tokenList, topLevel) {
 	}
 
 	// There could be a last token to add
-	if(token.length > 0) {
-		// In theory, no need to call _setTextOrBinaryOp(), as an "and" or "or" at the end of
-		// the queryString can't be a binary operator. On the other hand, we'd like this condition
-		// to be identifiable in a log, and not just silently ignored, so we'll call _setTextOrBinaryOp()
-		// to let the following logic flag this case.
-		tokenList.push(this._setTextOrBinaryOp(token.toLowerCase()));
-	}
+	generateToken(Classes.SearchTokenizer.type.TEXT);
 
 	if(!topLevel) {
 		// We get here if there's no closing parenthesis
 		this._log(logHead + "no matching closing \")\"");
 		return "";
+	}
+},
+
+// "quoteChar" is optional, and only used when tokenType = Classes.SearchTokenizer.type.QUOTEDTEXT
+getEscapedCharsList: function(tokenType, quoteChar) {
+	switch(tokenType) {
+		case Classes.SearchTokenizer.type.TEXT:
+			return this._escapedCharsList;
+
+		case Classes.SearchTokenizer.type.REGEX:
+			return [ " " ];
+
+		case Classes.SearchTokenizer.type.QUOTEDTEXT:
+			return [ optionalWithDefault(quoteChar, "\"") ];
+
+		case Classes.SearchTokenizer.type.SUBTREE:
+		case Classes.SearchTokenizer.type.BINARYOP:
+		case Classes.SearchTokenizer.type.UNARYOP:
+		default:
+			// Note that getEscapedCharsList() should not be called for tokenType
+			// Classes.SearchTokenizer.type.SUBTREE/BINARYOP/UNARYOP
+			const logHead = "SearchTokenizer::getEscapedCharsList(): ";
+			this._err(logHead + "invalid or unknown tokenType \"" + tokenType + "\"");
 	}
 },
 
