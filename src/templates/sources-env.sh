bf60d7ea5c961a6e1a9e5748fff9ace5a8cc2d1f
Marco Di Benedetto <marcodb.dev@gmail.com>
2021-03-30 11:54:35 -0700

    More tab -> bsTab cleanup

--- src/templates/sources-env.sh
+++ src/templates/sources-env.sh
@@ -13,7 +13,7 @@ declare UNPACKED_POPUP_SOURCES=(
 	# Don't include staging/staging.js unless you'r taking screenshots of TabMania for publishing
 	#	"../staging/TmStaging.js"																	
 	"SearchTokenizer.js" "SearchParser.js" "SearchOptimizer.js" "SearchQuery.js" "BookmarksManager.js"
-	"HistoryFinder.js" "PopupDocker.js" "PopupMsgServer.js" "icons.js" "Viewer.js" "TabViewer.js"
+	"HistoryFinder.js" "PopupDocker.js" "PopupMsgServer.js" "icons.js" "Viewer.js" "BsTabViewer.js"
 	"GroupsBuilder.js" "ContainerViewer.js" "SettingsItemViewer.js" "SettingsCustomGroupViewer.js"
 	"SettingsTabViewer.js" "PopupViewer.js" "TabsTitleMonitor.js" "TabsBsTabViewer.js" "TabTileViewer.js"
 	"TileMenuViewer.js" "PopupMenuViewer.js" "NewTabAction.js" "popup.js"
